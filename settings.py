072c763345ad3f1fc0d02d1629137a6ea44bf7a7
Anusha <ravillaanusha0@gmail.com>
2026-06-28 15:40:24 +0530

    Reorganize SECRET_KEY in settings.py
    Moved SECRET_KEY to a new line for clarity.

--- settings.py
+++ settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yh=@_+g^&f#c(h_&^m1$$vf%xum29okeqzun8zg_gsfu7i=hcw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+SECRET_KEY = 'django-insecure-yh=@_+g^&f#c(h_&^m1$$vf%xum29okeqzun8zg_gsfu7i=hcw'
 
 
 # Application definition
